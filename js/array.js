4f8fd6334263bd652bd11bcc1259716ef8c42039
Marcel Remmert <marcelremmert@web.de>
2023-05-03 11:42:44 +0200

    create function addName

--- js/array.js
+++ js/array.js
@@ -3,3 +3,18 @@ let studentArray = ["Ali" , "Patrick" , "Michael" , "Christian"];
 studentArray.forEach(item => {
     studentUl.innerHTML += '<li>'+item+'</li>';
 });
+
+function addName() {
+    if (document.getElementById('studentName').value == '') {
+
+        window.alert("Bitte einen Namen ins Textfeld schreiben");
+        
+    }
+    else{
+    studentArray.push(document.getElementById('studentName').value);
+
+    studentUl.innerHTML += '<li>'+studentArray[studentArray.length-1]+'</li>';
+
+    document.getElementById('studentName').value = '';
+    }
+}
